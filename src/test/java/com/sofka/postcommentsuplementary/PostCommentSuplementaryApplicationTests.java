f84452193fbca7c6ce94ceca034c471ae10dbb63
felipe <pipega2000@hotmail.com>
2022-08-06 20:00:00 -0500

    packages and app.properties created

---
+++ src/test/java/com/sofka/postcommentsuplementary/PostCommentSuplementaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.postcommentsuplementary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostCommentSuplementaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
