ecce72a385c7982ac94354211fad49b122e7e387
felipe <pipega2000@hotmail.com>
2022-08-07 20:21:52 -0500

    user service createUser created but no tested

--- src/main/java/com/sofka/postcommentsuplementary/service/UserService.java
+++ src/main/java/com/sofka/postcommentsuplementary/service/UserService.java
@@ -1,4 +1,14 @@
 package com.sofka.postcommentsuplementary.service;
 
+
+import com.sofka.postcommentsuplementary.entity.UserLike;
+import com.sofka.postcommentsuplementary.entity.UserLikeDTO;
+
+import java.util.List;
+
 public interface UserService {
+
+    UserLikeDTO createUserLike(UserLikeDTO userLike);
+
+    List<UserLike> findAllUsers();
 }
--- src/main/java/com/sofka/postcommentsuplementary/service/UserServiceImpl.java
+++ src/main/java/com/sofka/postcommentsuplementary/service/UserServiceImpl.java
@@ -1,7 +1,32 @@
 package com.sofka.postcommentsuplementary.service;
 
+import com.sofka.postcommentsuplementary.entity.UserLike;
+import com.sofka.postcommentsuplementary.entity.UserLikeDTO;
+import com.sofka.postcommentsuplementary.mapper.MapperUserLike;
+import com.sofka.postcommentsuplementary.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService{
+
+
+    @Autowired
+    UserRepo userRepo;
+
+
+    @Override
+    public UserLikeDTO createUserLike(UserLikeDTO userLikeDTO) {
+
+        UserLike userLike = MapperUserLike.returnUserLike(userLikeDTO);
+        return MapperUserLike.returnUserLikeDTO(userRepo.save(userLike));
+
+    }
+
+    @Override
+    public List<UserLike> findAllUsers() {
+        return userRepo.findAll();
+    }
 }
